3897ae1773c084027e3bb65baa86163d757ff36e
Edwin Choi <edwin.choi.wdev@gmail.com>
2022-08-20 23:05:39 -0500

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,111 @@
+document.querySelector('#search-button').addEventListener('click', getCards)
 
-
-
-
-
-function getFetchColor(color){
-
+function getCards(){
+    const nameInput = document.querySelector('#name-search').value
     const url = `https://api.pokemontcg.io/v2/cards/?`
-  
-    fetch(url + `pageSize=5&random=true&contains=imageUrl&colors=${color}`)
-        .then(res => res.json()) // parse response as JSON
-        .then(data => {
-          console.log(data)
-          console.log(data.cards[0].name)
-          document.querySelector('#card1name').innerText = data.cards[0].name
-          document.querySelector('#card1img').src = data.cards[0].imageUrl
-          document.querySelector('#card2name').innerText = data.cards[1].name
-          document.querySelector('#card2img').src = data.cards[1].imageUrl
-          document.querySelector('#card3name').innerText = data.cards[2].name
-          document.querySelector('#card3img').src = data.cards[2].imageUrl
-          document.querySelector('#card4name').innerText = data.cards[3].name
-          document.querySelector('#card4img').src = data.cards[3].imageUrl
-          document.querySelector('#card5name').innerText = data.cards[4].name
-          document.querySelector('#card5img').src = data.cards[4].imageUrl
+
+    const typeInputColorless = document.querySelector('#colorless').checked
+    const typeInputPsychic = document.querySelector('#psychic').checked
+    const typeInputWater = document.querySelector('#water').checked
+    const typeInputFire = document.querySelector('#fire').checked
+    const typeInputGrass = document.querySelector('#grass').checked
+    const typeInputLightning = document.querySelector('#lightning').checked
+
+    let supertypeInput = ''
+    let subtypeInput = ''
+    let subtypeParam = ''
+
+    if (typeInputColorless == true) {
+        typeInput = document.querySelector('#colorless').value
+    } else if (typeInputPsychic == true) {
+        typeInput = document.querySelector('#psychic').value
+    } else if (typeInputWater == true) {
+        typeInput = document.querySelector('#water').value
+    } else if (typeInputFire == true) {
+        typeInput = document.querySelector('#fire').value
+    } else if (typeInputGrass == true) {
+        typeInput = document.querySelector('#grass').value
+    } else if (typeInputLightning == true) {
+        typeInput = document.querySelector('#lightning').value
+    } else {
+        typeInput = ''
+    }
+
+    const supertypeInputPokemon = document.querySelector('#card-type-pokemon').checked
+    const supertypeInputTrainer = document.querySelector('#card-type-trainer').checked
+    const supertypeInputEnergy = document.querySelector('#card-type-energy').checked
+
+    if (supertypeInputPokemon == true) {
+        supertypeInput = document.querySelector('#card-type-pokemon').value
+    } else if (supertypeInputTrainer == true) {
+        supertypeInput = document.querySelector('#card-type-trainer').value
+    } else if (supertypeInputEnergy == true) {
+        supertypeInput = document.querySelector('#card-type-energy').value
+    }
+    
+    const subtypeInputBase = document.querySelector('#base').checked
+    const subtypeInputJungle = document.querySelector('#jungle').checked
+    const subtypeInputFossil = document.querySelector('#fossil').checked
+
+    if (subtypeInputBase == true) {
+        subtypeInput = document.querySelector('#base').value
+        subtypeParam += ` set.id:base1 OR` 
+    }
+    if (subtypeInputJungle == true) {
+        subtypeInput = document.querySelector('#jungle').value
+        subtypeParam += ` set.id:base2 OR`
+    }
+    if (subtypeInputFossil == true) {
+        subtypeInput = document.querySelector('#fossil').value
+        subtypeParam += ` set.id:base3 OR`
+    }
+    if (subtypeParam == '') {
+        subtypeParam = ` (set.id:base1 OR set.id:base2 OR set.id:base3)`
+    } else {
+        subtypeParam = subtypeParam.slice(0, -2)
+    }
+
+    fetchURLText = url + `page=1&pageSize=20&orderBy=set&q=`
+    if (nameInput != '') {
+        fetchURLText += ` name:${nameInput}` 
+    }
+    if (typeInput != '') {
+        fetchURLText += ` types:${typeInput}` 
+    }
+    if (supertypeInput != '') {
+        fetchURLText += ` supertype:${supertypeInput}`
+    }
+    if (subtypeParam != '') {
+        fetchURLText += subtypeParam
+    }
+    
+    console.log(fetchURLText)
+
+    fetch(fetchURLText, {
+        headers: {
+            "X-Api-Key": "9aac7fc4-dfb9-41eb-ab2f-f30e2976bd08"
+        }
+    })
+
+        .then(res => res.json())
+        .then(responseData => {
+
+        console.log(responseData)
+        console.log(responseData.data[0].name)
+
+        const cardContainer = document.querySelector('#card-container')
+        cardContainer.innerText = ''
+        for (var i = 0; i < responseData.data.length; i++) {
+            const newCard = document.createElement('div')
+            const newCardImg = document.createElement('img')
+            // newCard.innerText = responseData.data[i].name
+            newCardImg.src = responseData.data[i].images.small
+            cardContainer.appendChild(newCard)
+            cardContainer.appendChild(newCardImg)
+          }
         })
         .catch(err => {
             console.log(`error ${err}`)
     });
   }
+
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require ('cors');
 const PORT = 8000
 
+app.use(cors())
+app.use(express.static('public'))
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
