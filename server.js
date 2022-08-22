42d7d102dca90f24f283f1ba5b86b2a09ebd1b50
Edwin Choi <edwin.choi.wdev@gmail.com>
2022-08-21 22:36:30 -0500

    changes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require ('cors');
+const MongoClient = require('mongodb').MongoClient
 const PORT = 8000
 
 app.use(cors())
@@ -10,15 +11,22 @@ app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
 
-app.get('/api/:cardSearch', (request, response) => {
-    const rappersName = request.params.cardSearch.toLowerCase()
-    if (rappers[rappersName]) {
-        response.json(rappers[rappersName])
+app.get('/api/:addCard', (request, response) => {
+    const addCards = request.params.addCard.toLowerCase()
+    if (pokemon[cardImage]) {
+        response.json(pokemon[cardImage])
     } else {
-        response.json(rappers['unknown'])
+        response.json(pokemon['unknown'])
     }
 })
 
 app.listen(process.env.PORT || PORT, () => {
     console.log(`The server is running on port ${PORT}! You better go catch it!`)
 })
+
+
+//When you click a card, add its data to mongoDB, then place it into the "deck" on the dom
+//When you click a card from the deck, remove its data from mongoDB, and then remove it from the deck
+//A completed deck must have exactly 60 cards
+//While building the deck, you cannot exceed 60 cards
+//You cannot have more than 4 of the same card in a single deck, with the exception being Energy cards
