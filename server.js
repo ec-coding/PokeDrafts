f7d0520fded3faad6bf7f803d1d90338342655b9
Edwin Choi <edwin.choi.wdev@gmail.com>
2022-08-23 19:42:07 -0500

    changes

--- server.js
+++ server.js
@@ -27,25 +27,25 @@ MongoClient.connect("mongodb+srv://Zolere:Yggdrasil99!!@deck-builder.vtmbkox.mon
                 })
         })
 
-        // app.put('/quotes', (req, res) => {
-        //     console.log(req.body)
-        //     cardCollection.findOneAndUpdate(
-        //         { name: 'Yoda' },
-        //         {
-        //             $set: { 
-        //                 name: req.body.name,
-        //                 quote: req.body.quote
-        //             }
-        //         },
-        //         {
-        //             upsert: true
-        //         }
-        //     )
-        //     .then(result => {
-        //         res.json('Success')
-        //     })
-        //     .catch(error => console.error(error))
-        // })
+        app.put('/quotes', (req, res) => {
+            console.log(req.body)
+            cardCollection.findOneAndUpdate(
+                { name: 'Yoda' },
+                {
+                    $set: { 
+                        name: req.body.name,
+                        quote: req.body.quote
+                    }
+                },
+                {
+                    upsert: true
+                }
+            )
+            .then(result => {
+                res.json('Success')
+            })
+            .catch(error => console.error(error))
+        })
 
         app.delete('/cards', (req, res) => {
             cardCollection.deleteOne(
@@ -70,14 +70,14 @@ MongoClient.connect("mongodb+srv://Zolere:Yggdrasil99!!@deck-builder.vtmbkox.mon
 
 
 
-// app.get('/api/:addCard', (request, response) => {
-//     const addCards = request.params.addCard.toLowerCase()
-//     if (pokemon[cardImage]) {
-//         response.json(pokemon[cardImage])
-//     } else {
-//         response.json(pokemon['unknown'])
-//     }
-// })
+app.get('/api/:addCard', (request, response) => {
+    const addCards = request.params.addCard.toLowerCase()
+    if (pokemon[cardImage]) {
+        response.json(pokemon[cardImage])
+    } else {
+        response.json(pokemon['unknown'])
+    }
+})
 
 
 //When you click a card, add its data to mongoDB, then place a copy of its image into the "deck" on the dom
